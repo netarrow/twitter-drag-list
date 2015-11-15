2ca8b0caf9f5e6afeb28802597632b8dc073a722
netarrow <netarrow89@hotmail.com>
2015-11-15 18:29:42 +0100

    tolto scroll orrizzontale liste. con wrap vengono portate a capo, forse più comodo durante il drag and drop

--- app.js
+++ app.js
@@ -3,14 +3,6 @@
 
     app = angular.module('dragList', []);
 
-    $(function() {
-
-        $("body").mousewheel(function(event, delta) {
-            this.scrollLeft -= (delta * 30);
-            event.preventDefault();
-        });
-    });
-
     app.controller('HomeController', ['$http', function($http) {
         var ctr = this;
         $http.get('http://localhost:8080/api/lists').success(function(response) {
