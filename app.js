8dcad7a7402a06eb14e2b08633629dd6720839bc
netarrow <netarrow89@hotmail.com>
2015-11-15 18:24:29 +0100

    visualizzate liste in orizzontale, scrolling orrizzontale tramite wheel del mouse con jquery. refactoring angular inistallato da npm

--- app.js
+++ app.js
@@ -3,11 +3,20 @@
 
     app = angular.module('dragList', []);
 
+    $(function() {
+
+        $("body").mousewheel(function(event, delta) {
+            this.scrollLeft -= (delta * 30);
+            event.preventDefault();
+        });
+    });
+
     app.controller('HomeController', ['$http', function($http) {
         var ctr = this;
         $http.get('http://localhost:8080/api/lists').success(function(response) {
             ctr.lists = JSON.parse(response);
+        }).error(function(error) {
+            ctr.error = error;
         });
     }]);
-
 })();
