f0ab392b611038ef987163f59d2cc37b959d443e
Matteo <>
2021-10-24 09:42:23 +0200

    fix angular calls

--- app.js
+++ app.js
@@ -6,29 +6,29 @@
     app.controller('HomeController', ['$http', function($http) {
         var ctr = this;
 
-        $http.get('http://localhost:8080/api/lists').success(function(response) {
-            ctr.lists = JSON.parse(response);
+        $http.get('http://localhost:8080/api/lists').then(function(response) {
+            ctr.lists = JSON.parse(response.data);
 
             for(var i = 0; i < ctr.lists.length; i++) {
                 var id = ctr.lists[i].id;
 
                 (function(i) {
-                    $http.get('http://localhost:8080/api/members/' + id).success(function (response) {
-                        ctr.lists[i].users = JSON.parse(response).users;
-                    }).error(function (error) {
+                    $http.get('http://localhost:8080/api/members/' + id).then(function (response) {
+                        ctr.lists[i].users = JSON.parse(response.data).users;
+                    }).catch(function (error) {
                         ctr.error = error;
                     });
                 })(i);
 
             }
 
-        }).error(function(error) {
+        }).catch(function(error) {
             ctr.error = error;
         });
 
-        $http.get('http://localhost:8080/api/following').success(function(response) {
-            ctr.users = JSON.parse(response).users;
-        }).error(function(error) {
+        $http.get('http://localhost:8080/api/following').then(function(response) {
+            ctr.users = JSON.parse(response.data).users;
+        }).catch(function(error) {
             ctr.error = error;
         });
 
